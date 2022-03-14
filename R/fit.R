829fdef3fa0bdb30e42a3d97e9d12486d898f159
Sean Anderson <sean@seananderson.ca>
2022-03-14 16:27:47 -0700

    Set adapt_delta to higher value by default

--- R/fit.R
+++ R/fit.R
@@ -3,16 +3,18 @@
 #' @param data Stan data
 #' @param chains Chains
 #' @param iter Iterations
-#' @param ... Other arguments to pass to [cmdstanr::cmdstan_model()]
+#' @param adapt_delta Adapt delta argument for sampling
+#' @param ... Other arguments to pass to [cmdstanr::cmdstan_model()]. See the `$sampling()` method link from [cmdstanr::CmdStanModel()].
 #'
 #' @export
-fit_sarla <- function(data, chains = 4, iter = 2000, ...) {
+fit_sarla <- function(data, chains = 4, iter = 2000, adapt_delta = 0.95, ...) {
   mod <- cmdstanr::cmdstan_model(
     system.file("stan", "sarla.stan", package = "sarla")
   )
   fit <- mod$sample(
     data = data,
     chains = chains,
+    adapt_delta = adapt_delta,
     ...
   )
   fit
